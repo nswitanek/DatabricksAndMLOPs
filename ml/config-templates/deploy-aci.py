a49cdeb77ae2754beb41b8d84e9a21f24877d7a8
Will Johnson <wijohns@microsoft.com>
2020-12-05 10:11:17 -0600

    Finalizing the deployment step

--- ml/config-templates/deploy-aci.py
+++ ml/config-templates/deploy-aci.py
@@ -3,11 +3,10 @@ import os
 
 from azureml.core import Model, Workspace
 from azureml.core.authentication import ServicePrincipalAuthentication
+from azureml.core.conda_dependencies import CondaDependencies
 from azureml.core.environment import Environment
 from azureml.core.model import InferenceConfig
-from azureml.core.webservice import AciWebservice, AksWebservice, LocalWebservice
-
-from azureml.core.webservice import LocalWebservice, Webservice
+from azureml.core.webservice import AciWebservice, AksWebservice
 
 
 if __name__ == "__main__":
@@ -20,29 +19,39 @@ if __name__ == "__main__":
     
     # Authenticate to AML Workspace
     sp = ServicePrincipalAuthentication(
-            tenant_id=os.environ.get("TENANT_ID"),
-            service_principal_id=os.environ.get("CLIENT_ID"),
-            service_principal_password=os.environ.get("CLIENT_SECRET")
+        tenant_id=os.environ.get("TENANT_ID"),
+        service_principal_id=os.environ.get("CLIENT_ID"),
+        service_principal_password=os.environ.get("CLIENT_SECRET")
     ) 
 
     print("Connecting to AML Workspace")
     ws = Workspace.get(
-            name=os.environ.get("AML_WORKSPACE_NAME"),
-            subscription_id=os.environ.get("AML_SUBSCRIPTION_ID"),
-            auth=sp
+        name=os.environ.get("AML_WORKSPACE_NAME"),
+        subscription_id=os.environ.get("AML_SUBSCRIPTION_ID"),
+        auth=sp
     )
 
-    env = Environment.get(ws, "AzureML-PySpark-MmlSpark-0.15").clone(args.environment_name)
+    env = Environment.get(ws, name='AzureML-PySpark-MmlSpark-0.15')
 
     inference_config = InferenceConfig(
         entry_script=args.scoring_script, 
-        environment=env
+        environment=env,
     )
 
     model = Model(ws, id=args.model_id)
 
+    # These should PySpark and Spark version 2.4.5
+    print(model.model_framework)
+    print(model.model_framework_version)
+
     deployment_config = AciWebservice.deploy_configuration(cpu_cores=1, memory_gb=2)
     
-    service = Model.deploy(ws, args.service_name, [model], inference_config, deployment_config)
-    service.wait_for_deployment(show_output = True)
+    service = Model.deploy(ws, args.service_name, [model], inference_config, deployment_config, overwrite=True)
+    try:
+        service.wait_for_deployment(show_output = True)
+    except Exception as e:
+        print("There was a failure")
+        print(service.get_logs())
+        print("="*10)
+        raise e
     print(service.state)
--- ml/score/score.py
+++ ml/score/score.py
@@ -1,25 +1,48 @@
+import traceback
 import json
 from azureml.core.model import Model
-from pyspark.sql import SparkSession
-from pyspark.ml import PipelineModel
-  
+from pyspark.ml.pipeline import PipelineModel
+from pyspark.sql.types import StructType, StructField
+from pyspark.sql.types import DoubleType, StringType, IntegerType, DecimalType
+from pyspark.sql import SQLContext
+from pyspark import SparkContext
+
+sc = SparkContext.getOrCreate()
+sqlContext = SQLContext(sc)
+spark = sqlContext.sparkSession
+
+schema = StructType([
+  StructField('YearBuilt', StringType(), True),
+  StructField('Bedrooms', IntegerType(), True),
+  StructField('Bathrooms', DecimalType(), True),
+  StructField('Square Footage', IntegerType(), True),
+  StructField('AvgSchoolRanking', DecimalType(), True)
+])
+reader = spark.read
+reader.schema(schema)
+
+
 def init():
-  global spark
-  global loaded_model
-  spark = SparkSession.builder.appName("home_pricing").getOrCreate()
-  model_path = Model.get_model_path(model_name="dummy1")
-  loaded_model = PipelineModel.load(model_path)
- 
-def run(raw_data):
-  try:
-    input_list = json.loads(raw_data)["data"]
-    sc = spark.sparkContext
-    input_rdd = sc.parallelize(input_list)
-    input_df = input_rdd.toDF()
-    pred_df = loaded_model.transform(input_df)
-    pred_list = pred_df.collect()
-    pred_array = [int(x["prediction"]) for x in pred_list]
-    return pred_array
-  except Exception as e:
-    result = str(e)
-    return "Internal Exception : " + result
+    global model
+    # Get the model by name registered in Azure ML
+    # This should get replaced by the devops pipeline
+    model_path = Model.get_model_path('{{MODEL_NAME_TO_BE_REPLACED_BY_SED}}')
+    model = PipelineModel.load(model_path)
+
+
+def run(input_data):
+    output = {}
+    output["input"] = input_data
+    try:
+        input_dict = json.loads(input_data)
+        input_df = reader.json(sc.parallelize(input_dict["data"]))
+        result = model.transform(input_df)
+        # you can return any datatype as long as it is JSON-serializable
+        value = [p.asDict()['prediction'] for p in result.collect()]
+        output["predictions"] = value
+    except Exception as e:
+        traceback.print_exc()
+        error = str(e)
+        output["error"] = error
+    
+    return output
